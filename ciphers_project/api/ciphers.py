a6e7f5acc6bcb102c8994ed7696abdd38112d210
7konigsberg <77811205+7konigsberg@users.noreply.github.com>
2024-02-27 19:11:55 -0500

    first commit

---
+++ ciphers_project/api/ciphers.py
@@ -0,0 +1,19 @@
+# def caesar_encode(plain_text, shift):
+#     cipher_text = ""
+#     for c in plain_text:
+#         c_encoded = ord(c) + shift
+#         c_encoded = chr(c_encoded)
+#         cipher_text += c_encoded
+#     return cipher_text
+
+def caesar_encode(plain_text, shift):
+    cipher_text = ""
+    for c in plain_text:
+        if c.isupper():  # check if it's an uppercase character
+            c_encoded = ((ord(c) - ord('A') + shift) % 26) + ord('A')
+        elif c.islower():  # check if it's a lowercase character
+            c_encoded = ((ord(c) - ord('a') + shift) % 26) + ord('a')
+        else:
+            c_encoded = ord(c)  # non-alphabet characters are not encoded
+        cipher_text += chr(c_encoded)
+    return cipher_text
